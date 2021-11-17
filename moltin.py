522e11d2700398d1877d6602fe10a7b8db989fa1
acostyle <acostyle.m.z@protonmail.com>
2021-11-17 17:32:49 +0600

    feat: add def delete_product_from_cart

--- moltin.py
+++ moltin.py
@@ -107,6 +107,21 @@ def get_cart_items(access_token, cart_id):
     return response.json()
 
 
+def delete_product_from_cart(access_token, cart_id, product_id):
+    headers = {
+        'Authorization': 'Bearer {0}'.format(access_token),
+    }
+    api_url = '{0}/v2/carts/{1}/items/{2}'.format(
+        API_BASE_URL,
+        cart_id,
+        product_id,
+    )
+    response = requests.delete(url=api_url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()
+
+
 def main():
     access_token = get_auth_token()
 
